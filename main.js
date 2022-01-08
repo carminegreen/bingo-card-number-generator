8393c52f629257ce958163bd03480a3e33fd6e8f
carminegreen <tec.carmineverde@gmail.com>
2022-01-08 21:41:00 +0100

    (working): unique number in a series

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { range, rand} =  require("./utils/range");
 const columnsValueLength = [9,10,10,10,10,10,10,10,11]
 const numberFolder = 6;
 
-const totalNumber =  range(1,91)
+const totalNumber =  range(1,91,1)
 
 if (numberFolder % 6 !== 0) {
     return console.log("Inserire come numberFolder un multiplo di 6")
@@ -22,42 +22,35 @@ for (let i = 0; i < numberFolder/6; i++) {
         range(50,60,1),
         range(60,70,1),
         range(70,80,1),
-        range(80,91,1)];
+        range(80,91,1)
+    ];
+
+    // columnsValue.forEach((row,i)  => {
+    //
+    // })
 
     for (let f = 0; f < 6; f++) {
 
         for (let j = 0; j < 3; j++) {
             const row = [];
-            columnsValue.map(group => {
-                const value = addNumber(group, row);
-                columnsValue.forEach(row => {
-                    if(row.includes(value)) {
-                        row.splice(row.findIndex(r => r !== value))
-                    }
-                })
-                //columnsValue = columnsValue.map(row => row.filter(r => r !== value))
+            columnsValue.forEach((group, i) => {
+                addNumber(group, row)
             });
             // prendi 5 numeri per ogni riga
             const values = row.sort(() => .5 - Math.random()).slice(0,5).sort((a, b) => a-b );
+            console.log(values.map(value => columnsValue.find(row => row.find(v => v === value))))
             folder.push({series: i+1, folder: f+1,  values});
             //console.log(columnsValue)
         }
 
     }
-    console.log(columnsValue)
-    console.log(folder)
+    // console.log(columnsValue)
+     console.log(folder)
 
 }
 
 
 function addNumber(row, array) {
     const casualNumber = Math.floor(Math.random()* row.length);
-    // excluded.forEach(row => {
-    //     if(row.includes(casualNumber)) {
-    //         console.log("è successo con il numero", casualNumber)
-    //         //addNumber(row, array, excluded);
-    //     }
-    // });
     array.push(row[casualNumber]);
-    return casualNumber;
 }
