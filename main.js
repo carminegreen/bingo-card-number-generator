3383279fd4a6994f8533dfbe13a1c00a4ef945c9
carminegreen <tec.carmineverde@gmail.com>
2022-01-09 15:31:17 +0100

    (feat): Add main

--- main.js
+++ main.js
@@ -1,55 +1,60 @@
-const { range} =  require("./utils/range");
+const { range } =  require("./utils/range");
 
-// const columnsValueLength = [9,10,10,10,10,10,10,10,11]
-const numberFolder = 6;
+// Start
+main();
 
-if (numberFolder % 6 !== 0) {
-    return console.log("Inserire come numberFolder un multiplo di 6")
-}
+function main() {
 
-for (let i = 0; i < numberFolder/6; i++) {
-    let folder = [];
-    let columnsValue = [
-        range(1,10,1),
-        range(10,20,1),
-        range(20,30,1),
-        range(30,40,1),
-        range(40,50,1),
-        range(50,60,1),
-        range(60,70,1),
-        range(70,80,1),
-        range(80,91,1)
-    ];
+    // const columnsValueLength = [9,10,10,10,10,10,10,10,11]
+    const numberFolder = 6;
 
-    console.log(columnsValue)
+    if (numberFolder % 6 !== 0) {
+        return console.log("Inserire come numberFolder un multiplo di 6")
+    }
 
-    for (let f = 0; f < 6; f++) {
+    for (let i = 0; i < numberFolder/6; i++) {
+        const folder = [];
+        let columnsValue = [
+            range(1,10,1),
+            range(10,20,1),
+            range(20,30,1),
+            range(30,40,1),
+            range(40,50,1),
+            range(50,60,1),
+            range(60,70,1),
+            range(70,80,1),
+            range(80,91,1)
+        ];
 
-        for (let j = 0; j < 3; j++) {
-            const row = [];
-            columnsValue.forEach((group, i) => {
-                addNumber(group.filter(v => !!v), row)
-            });
-            // prendi 5 numeri per ogni riga
-            const values = row.sort(() => .5 - Math.random()).sort(() => .5 - Math.random()).slice(2,7).sort((a, b) => a-b );
-            values.forEach(value => columnsValue.forEach(row => {
-                const v = row.findIndex(v => v === value);
-                if (v !== -1) {
-                    columnsValue[columnsValue.indexOf(row)] = row.filter(r => r !== value)
-                }
-            }));
-            folder.push({series: i+1, folder: f+1,  values});
-            //console.log(columnsValue)
+        for (let f = 0; f < 6; f++) {
+
+            for (let j = 0; j < 3; j++) {
+                const result = [];
+                columnsValue.forEach(row => addNumber(row, result) );
+                // prendi 5 numeri per ogni riga
+                const values = result.sort(() => .5 - Math.random()).sort(() => .5 - Math.random()).slice(0,5).sort((a, b) => a-b );
+                folder.push({series: i+1, folder: f+1,  values});
+                values.forEach(value => columnsValue.forEach(row => {
+                    const v = row.findIndex(v => v === value);
+                    if (v !== -1) {
+                        columnsValue[columnsValue.indexOf(row)] = row.filter(r => r !== value)
+                    }
+                }));
+            }
+
         }
+        // console.log(columnsValue)
+        console.log(folder)
 
     }
-    // console.log(columnsValue)
-    // console.log(folder)
-
 }
 
 
 function addNumber(row, result) {
+    if(row.length === 0) {
+        return;
+    }
     const casualNumber = Math.floor(Math.random()* row.length);
+
     result.push(row[casualNumber]);
 }
