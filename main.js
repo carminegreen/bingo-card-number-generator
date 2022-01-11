3975531a3927faff171135d130efe49a5354d5b8
carminegreen <tec.carmineverde@gmail.com>
2022-01-12 00:19:05 +0100

    (feat): Beautify code, add check of unique folder, update result array and add replace number to constant

--- main.js
+++ main.js
@@ -6,16 +6,19 @@ main();
 
 function main() {
 
-    // const columnsValueLength = [9,10,10,10,10,10,10,10,11]
+    // const columnsValueLength = [9,10,10,10,10,10,10,10,11];
     const numberFolder = process.argv[2] || 6;
+    const FOLDER_LENGTH = 6;
+    const ROW_LENGTH = 3;
+    const NUMBER_OF_ROW = 5;
 
-    if (numberFolder % 6 !== 0) {
-        return console.log("Inserire come numberFolder un multiplo di 6")
+    if (numberFolder % FOLDER_LENGTH !== 0) {
+        return console.log("Inserire come numberFolder un multiplo di FOLDER_LENGTH");
     }
 
     const series = [];
 
-    for (let i = 0; i < numberFolder/6; i++) {
+    for (let i = 0; i < numberFolder/FOLDER_LENGTH; i++) {
         const folders = [];
         let columnsValue = [
             range(1,10,1),
@@ -29,42 +32,59 @@ function main() {
             range(80,91,1)
         ];
 
-        for (let f = 0; f < 6; f++) {
+        for (let f = 0; f < FOLDER_LENGTH; f++) {
             const folder = [];
 
-            for (let j = 0; j < 3; j++) {
+            for (let j = 0; j < ROW_LENGTH; j++) {
                 const result = [];
 
                 columnsValue.map((r, i) => ({ len: r.length, index:i })).sort((a,b) => b.len-a.len)
-                    .slice(0,5).sort(() => .5 - Math.random()).forEach(item => {
-                    addNumber(columnsValue[item.index], result);
+                    .slice(0,NUMBER_OF_ROW).sort(() => .5 - Math.random()).forEach(item => {
+
+                        // addNumber(columnsValue[item.index], result);
+                        const random = Math.floor(Math.random()* columnsValue[item.index].length);
+                        const value = columnsValue[item.index][random];
+                        result.push(value);
+
+                        columnsValue[item.index] = columnsValue[item.index].filter(r => r !== value);
                 });
 
-                folder.push(result);
-                result.forEach(value => columnsValue.forEach(row => {
-                    const v = row.findIndex(v => v === value);
-                    if (v !== -1) {
-                        columnsValue[columnsValue.indexOf(row)] = row.filter(r => r !== value)
-                    }
-                }));
+                const folderOrder = result.sort((a,b) => a - b);
+
+                folder.push(folderOrder);
+
             }
-            folders.push(folder);
+            const number = i === 0 ? f+1 : ((FOLDER_LENGTH * i) + f + 1);
+            folders.push({number, folder});
 
         }
+        // console.log(columnsValue)
+        console.log(`Generate ${i+1} serie su ${numberFolder/FOLDER_LENGTH}`);
+
+
+        if(series.map(arr => arr.folders.map(f => folders.find(folder => JSON.stringify(f) === JSON.stringify(folder)))).filter(v => !v).length !== 0) {
+            writeJson(series, FOLDER_LENGTH);
+            process.exit(1);
+        }
+
         series.push({number: i+1, folders});
 
     }
-    fs.writeFile("./folders.json", JSON.stringify(series, null, 4), (err) => {
-        if (err) {  console.error(err);  return; };
-        console.log(`${numberFolder} folders were successfully generated!`);
+    writeJson(series, FOLDER_LENGTH);
+}
+
+function writeJson(data, dividend) {
+    fs.writeFile("./folders.json", JSON.stringify(data, null, 4), (err) => {
+        if (err) {  console.error(err);  return; }
+        console.log(`${data.length * dividend} folders were successfully generated!`);
     });
 }
 
 
-function addNumber(row, result) {
-    if(row.length === 0) {
-        return;
-    }
-    const casualNumber = Math.floor(Math.random()* row.length);
-    result.push(row[casualNumber]);
-}
+// function addNumber(row, result) {
+//     if(row.length === 0) {
+//         return;
+//     }
+//     const casualNumber = Math.floor(Math.random()* row.length);
+//     result.push(row[casualNumber]);
+// }
